f9d07a1baeb79a21401d159187dafa1a754eb815
zhangyanling <18783226594@163.com>
2020-03-28 16:09:10 +0800

    init

---
+++ karma.conf.js
@@ -0,0 +1,15 @@
+var webpackConfig = require('@vue/cli-service/webpack.config')
+
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha'],
+    files: ['tests/**/*.spec.js'],
+    preprocessors: {
+      '**/*.spec.js': ['webpack', 'sourcemap']
+    },
+    autoWatch: true,
+    webpack: webpackConfig,
+    reporters: ['spec'],
+    browsers: ['ChromeHeadless']
+  })
+}
