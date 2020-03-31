34a5b60a03f92d50260b10fb75c0de2df3e68a4c
zhangyanling77 <18783226594@163.com>
2020-03-31 12:51:05 +0800

    feat：row

---
+++ src/packages/layout/row.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="zyl-row" :style="rowStyle">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'zyl-row',
+  props: {
+    gutter: {
+      type: Number,
+      default: 0
+    }
+  },
+  mounted() {
+    this.$children.forEach(child => {
+      child.gutter = this.gutter
+    })
+  },
+  computed: {
+    rowStyle() {
+      let style = {}
+      if (this.gutter) {
+        style = {
+          ...style,
+          marginLeft: -this.gutter / 2 + 'px',
+          marginRight: -this.gutter / 2 + 'px'
+        }
+      }
+      return style
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.zyl-row {
+  display: flex;
+}
+</style>
