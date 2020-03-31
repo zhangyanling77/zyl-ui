bd6c4ff53e76e45ddba6290e62cb5fedb67659c3
zhangyanling77 <18783226594@163.com>
2020-03-31 12:54:18 +0800

    feat: demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,113 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Button 按钮</h1>
+    <div class="block">
+      <!-- 默认按钮 -->
+      <zyl-button>默认按钮</zyl-button>
+    </div>
+    <div class="block">
+      <!-- 带类型的按钮 -->
+      <zyl-button type="primary">主要按钮</zyl-button>
+      <zyl-button type="success">成功按钮</zyl-button>
+      <zyl-button type="info">信息按钮</zyl-button>
+      <zyl-button type="warning">警告按钮</zyl-button>
+      <zyl-button type="danger">危险按钮</zyl-button>
+    </div>
+    <div class="block">
+      <!-- 带icon的按钮 -->
+      <zyl-button icon="edit" type="primary">主要按钮</zyl-button>
+      <zyl-button icon="edit" type="success" icon-position="right">危险按钮</zyl-button>
+      <zyl-button icon="search" type="danger"></zyl-button>
+    </div>
+    <div class="block">
+      <!-- loading按钮 -->
+      <zyl-button type="primary" loading>加载中...</zyl-button>
+    </div>
+    <div class="block">
+      <!-- 触发事件 -->
+      <zyl-button type="primary" @click="fn">触发事件</zyl-button>
+    </div>
+    <div class="block">
+      <zyl-button-group>
+        <zyl-button type="primary" icon="left" icon-position="left">上一页</zyl-button>
+        <zyl-button type="primary" icon="right" icon-position="right">下一页</zyl-button>
+      </zyl-button-group>
+    </div>
+    <h1>Icon 图标</h1>
+    <div class="block">
+      <!-- 图标 -->
+      <zyl-icon icon="search"></zyl-icon>
+      <zyl-icon icon="edit"></zyl-icon>
+      <zyl-icon icon="heart"></zyl-icon>
+    </div>
+    <h1>Layout 布局</h1>
+    <div class="block">
+      <!-- 布局组件 Row Col -->
+      <zyl-row>
+        <zyl-col :span="12">
+          <div class="red">col-12</div>
+        </zyl-col>
+        <zyl-col :span="12">
+          <div class="green">col-12</div>
+        </zyl-col>
+      </zyl-row>
+      <zyl-row>
+        <zyl-col :span="24">
+          <div class="yellow">col-24</div>
+        </zyl-col>
+      </zyl-row>
+      <zyl-row>
+        <zyl-col :span="6">
+          <div class="red">col-6</div>
+        </zyl-col>
+        <zyl-col :span="6" :offset="12">
+          <div class="green">col-6</div>
+        </zyl-col>
+      </zyl-row>
+      <zyl-row :gutter="16">
+        <zyl-col :span="6">
+          <div class="red">col-12</div>
+        </zyl-col>
+        <zyl-col :span="6">
+          <div class="green">col-12</div>
+        </zyl-col>
+        <zyl-col :span="6">
+          <div class="red">col-12</div>
+        </zyl-col>
+        <zyl-col :span="6">
+          <div class="green">col-12</div>
+        </zyl-col>
+      </zyl-row>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    fn(e) {
+      console.log(e)
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.block {
+  padding: 20px;
+  >button, >svg{
+    margin-right: 16px;
+  }
+  .red {
+    background-color: crimson;
+  }
+  .green {
+    background-color: greenyellow;
+  }
+  .yellow {
+    background-color: darkorange;
+  }
 }
 </style>
