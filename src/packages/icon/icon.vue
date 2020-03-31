38956e42df8af951d33681a2615eb5e6d9d8e1b6
zhangyanling77 <18783226594@163.com>
2020-03-31 12:49:52 +0800

    feat: icon

---
+++ src/packages/icon/icon.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg class="zyl-icon" aria-hidden="true">
+    <use :xlink:href="`#icon-${icon}`"></use>
+  </svg>
+</template>
+
+<script>
+import '@/styles/icon';
+export default {
+  name: 'zyl-icon',
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .zyl-icon{
+    width: 24px;
+    height:24px;
+    vertical-align: middle;
+  }
+</style>
