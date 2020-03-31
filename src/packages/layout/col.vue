3c1849c614d6f0e052458f06f9c2a630618a5d56
zhangyanling77 <18783226594@163.com>
2020-03-31 12:50:36 +0800

    feat: col

---
+++ src/packages/layout/col.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="zyl-col" :class="colClass" :style="colStyle">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'zyl-col',
+  data() {
+    return {
+      gutter: 0
+    }
+  },
+  props: {
+    span: {
+      type: Number,
+      default: 24
+    },
+    offset: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    colClass() { // 统一管理样式
+      let classes = []
+      classes.push(`zyl-col-${this.span}`)
+      if (this.offset) {
+        classes.push(`zyl-col-offset-${this.offset}`)
+      }
+      return classes
+    },
+    colStyle() {
+      let style = {}
+      if (this.gutter) {
+        style = { 
+          ...style,
+          paddingLeft: this.gutter / 2 + 'px',
+          paddingRight: this.gutter / 2 + 'px',
+        }
+      }
+      return style
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@for $i from 1 through 24 {
+  .zyl-col-#{$i} {
+    width: $i / 24 * 100%;
+  }
+  .zyl-col-offset-#{$i} {
+    margin-left: $i / 24 * 100%;
+  }
+}
+
+</style>
