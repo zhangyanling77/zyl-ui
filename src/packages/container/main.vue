68a11bfe0b171c294b6653b2c058024ccdc441d9
zhangyanling <18783226594@163.com>
2020-04-03 20:52:47 +0800

    feat:main

--- src/packages/container/main.vue
+++ src/packages/container/main.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
-    main
-  </div>
+  <main class="zyl-main">
+    <slot></slot>
+  </main>
 </template>
-
 <script>
 export default {
-  name: 'zyl-main'
+  name:'zyl-main'
 }
 </script>
 
 <style lang="scss">
-
+.zyl-main{
+  flex:1;
+  padding:20px;
+}
 </style>
