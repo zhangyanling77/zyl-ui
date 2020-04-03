4d0e1fab87ea9b5ebce799b68064f843a8d91f9b
zhangyanling <18783226594@163.com>
2020-04-03 20:51:16 +0800

    feat:header/footer

--- src/packages/container/footer.vue
+++ src/packages/container/footer.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
-    footer
-  </div>
+    <footer class="zyl-footer">
+        <slot></slot>
+    </footer>
 </template>
 
 <script>
 export default {
-  name: 'zyl-footer'
+    name:'zyl-footer'
 }
 </script>
 
 <style lang="scss">
-
+.zyl-footer{
+    height:60px
+}
 </style>
--- src/packages/container/header.vue
+++ src/packages/container/header.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
-    header
-  </div>
+    <header class="zyl-header">
+        <slot></slot>
+    </header>
 </template>
 
 <script>
 export default {
-  name: 'zyl-header'
+    name:'zyl-header'
 }
 </script>
 
 <style lang="scss">
-
+.zyl-header{
+    height:60px
+}
 </style>
