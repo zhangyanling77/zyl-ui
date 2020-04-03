c2a11da6f710ae7df379a132d5ede52bbda72bbd
zhangyanling <18783226594@163.com>
2020-04-03 20:47:27 +0800

    feat:congtainer

--- src/packages/container/container.vue
+++ src/packages/container/container.vue
@@ -1,15 +1,32 @@
 <template>
-  <section class="zyl-container">
+  <section class="zyl-container" :class="{isVertical}">
     <slot></slot>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'zyl-container'
+  name: 'zyl-container',
+  data() {
+    return {
+      isVertical: true
+    }
+  },
+  mounted() {
+    this.isVertical = this.$children.some(child =>
+      ["zyl-footer", "zyl-header"].includes(child.$options.name)
+    );
+  }
 }
 </script>
 
 <style lang="scss">
-
+.zyl-container {
+  display: flex;
+  flex-direction: row;
+  flex:1
+}
+.zyl-container .isVertical {
+  flex-direction: column;
+}
 </style>
