577de3b7ddf2a262d82ae2a6cbcb5e940d5d4c59
zhangyanling <18783226594@163.com>
2020-04-22 20:45:51 +0800

    feat: add dir

---
+++ src/packages/pagination/pagination.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    pagination
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'zyl-pagination'
+}
+</script>
+
+<style lang="scss">
+  
+</style>
---
+++ src/packages/table/table.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    table
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'zyl-table'
+}
+</script>
+
+<style lang="scss">
+  
+</style>
