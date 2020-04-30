1249b37bc145375cb55e701bb4bf124752d954ad
zhangyanling77 <18783226594@163.com>
2020-04-30 18:40:57 +0800

    feat: table init

--- src/packages/table/table.vue
+++ src/packages/table/table.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
-    table
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'zyl-table'
+  name: 'zyl-table',
+  props: {
+    columns: {
+      type: Array,
+      default: () => [],
+    },
+    data: {
+      type: Array,
+      default: () => [],
+    }
+  },
 }
 </script>
 
 <style lang="scss">
-  
+
 </style>
