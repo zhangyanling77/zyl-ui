4e938d1e0afe569fb78b426b377a64f0c521a583
zhangyanling <18783226594@163.com>
2020-05-02 12:52:37 +0800

    feat: 全选、半选

--- src/packages/table/table.vue
+++ src/packages/table/table.vue
@@ -5,9 +5,11 @@
         <tr>
           <th v-for="(col, index) in cloneColumns" :key="index" :style="{width:col.width+'px'}">
             <div v-if="col.type == 'selection'">
-              <input type="checkbox" />
+              <input type="checkbox" :checked="checkAllStatus" ref="checkAll" @change="selectAll" />
             </div>
-            {{col.title}}
+            <div v-else>
+              {{col.title}}
+            </div>
           </th>
         </tr>
       </thead>
@@ -15,9 +17,9 @@
         <tr v-for="(row, index) in cloneData" :key="index">
           <td v-for="(col, index) in cloneColumns" :key="index">
             <div v-if="col.type == 'selection'">
-              <input type="checkbox" />
+              <input type="checkbox" @change="selectOne($event,row)" :checked="isChecked(row)" />
             </div>
-            <div>
+            <div v-else>
               {{row[col.key]}}
             </div>
           </td>
@@ -45,8 +47,49 @@ export default {
     return {
       cloneColumns: cloneDeep(this.columns),
       cloneData: cloneDeep(this.data),
+      selectedItems: [],
     };
   },
+  watch: {
+    selectedItems() {
+      // 我要设置半选状态
+      if (this.cloneData.length != this.selectedItems.length) {
+        if (this.selectedItems.length > 0) {
+          return (this.$refs.checkAll[0].indeterminate = true);
+        }
+      }
+      this.$refs.checkAll[0].indeterminate = false; // 设置半选状态，indeterminate是js原生属性
+    }
+  },
+  computed: {
+    checkAllStatus() {
+      return this.cloneData.length == this.selectedItems.length;
+    }
+  }, 
+  created() {
+    // 给每一条数据添加一个唯一标识
+    this.cloneData = this.cloneData.map(row => {
+      row._id = Math.random();
+      return row;
+    });
+  },
+  methods: {
+    isChecked(row) {
+      return this.selectedItems.some(r => r._id == row._id);
+    },
+    selectAll(e) {
+      this.selectedItems = e.target.checked ? this.cloneData : [];
+      this.$emit("on-select-all", this.selectedItems);
+    },
+    selectOne(e, row) {
+      if (e.target.checked) {
+        this.selectedItems.push(row);
+      } else {
+        this.selectedItems = this.selectedItems.filter(r => r._id != row._id);
+      }
+      this.$emit("on-select", this.selectedItems, row);
+    },
+  }
 }
 </script>
 
